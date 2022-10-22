9b4e7ef2cfe6c9b112a502da517272ba78f39d24
Pol Curto <pol.curto@students.salle.url.edu>
2022-10-22 12:39:41 +0200

    hitbox paddle

--- PaddleScript.cpp
+++ PaddleScript.cpp
@@ -10,24 +10,24 @@ void PaddleScript::tickScript(float deltaTime) {
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
 		transform->position += glm::vec2(1., 0.) * deltaTime / 2.f;
 
-		if (transform->position.x > 750) transform->position.x = 750;
+		if (transform->position.x > 1500) transform->position.x = 1500;
 	}
 
 	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
 		transform->position += glm::vec2(-1., 0.) * deltaTime / 2.f;
 
-		if (transform->position.x < 50) transform->position.x = 50;
+		if (transform->position.x < 100) transform->position.x = 100;
 	}
 
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
 		transform->position += glm::vec2(0., -1.) * deltaTime / 2.f;
 
-		if (transform->position.x > 750) transform->position.x = 750;
+		if (transform->position.y < 100) transform->position.y = 100;
 	}
 
 	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
 		transform->position += glm::vec2(0., 1.) * deltaTime / 2.f;
 
-		if (transform->position.x < 50) transform->position.x = 50;
+		if (transform->position.y > 700) transform->position.y = 700;
 	}
 }
